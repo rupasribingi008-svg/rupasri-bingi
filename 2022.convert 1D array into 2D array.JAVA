8bc2b6522a2704e374487dd5595ee1f3e3eeaf6b
rupasribingi008-svg <rupasribingi008@gmail.com>
2026-07-21 14:53:49 +0530

    Create 2022.convert 1D array into 2D array.JAVA

---
+++ 2022.convert 1D array into 2D array.JAVA
@@ -0,0 +1,12 @@
+class Solution {
+    public int[][] construct2DArray(int[] original, int m, int n) {
+        int k = original.length;
+        if (m * n != k)
+            return new int[0][0];
+        int[][] arr = new int[m][n];
+        for (int i = 0; i < m; i++)
+            for (int j = 0; j < n; j++)
+                arr[i][j] = original[i * n + j];
+        return arr;
+    }
+}
