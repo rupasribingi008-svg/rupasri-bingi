a533a504234b36a471f2407b551a5a9d9367c1af
rupasribingi008-svg <rupasribingi008@gmail.com>
2026-07-21 14:49:04 +0530

    Create 1769.minimum number of operations to move all balls to each box.JAVA

---
+++ 1769.minimum number of operations to move all balls to each box.JAVA
@@ -0,0 +1,16 @@
+class Solution {
+    public int[] minOperations(String boxes) {
+        char arr[] = boxes.toCharArray();
+        int ans[] = new int[arr.length];
+        for(int i = 0; i< arr.length; i++){
+            int sum = 0;
+            for(int j = 0; j< arr.length; j++){
+                if((j != i) && arr[j] =='1'){
+                    sum += Math.abs(i - j);
+                }
+            }
+            ans[i] = sum;
+        }
+        return ans;
+    }
+}
